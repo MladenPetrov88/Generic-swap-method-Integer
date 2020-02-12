17e98429a4c33aead3550001c9385feff9d2a4ba
Mladen Petrov <mladen.petrov88@gmail.com>
2020-02-12 11:50:41 +0200

    Add files via upload

---
+++ Box.java
@@ -0,0 +1,23 @@
+package GenericSwapMethodInteger;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Box<T> {
+    private ArrayList<T> arr = new ArrayList<>();
+
+    public void add (T element) {
+        this.arr.add(element);
+    }
+
+
+    public void print () {
+        arr.stream().forEach(e -> System.out.println("java.lang.Integer: " + e));
+    }
+
+    public void set(int index, int index1) {
+        Collections.swap(arr,index, index1);
+
+
+    }
+}
